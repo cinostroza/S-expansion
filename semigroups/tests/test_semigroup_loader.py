f213ea16cf8dfddb6a96836139398dd0b9d58d03
Carlos Inostroza <c.inostroza@gmail.com>
2023-03-29 21:27:29 -0300

    Extending unit tests for higer order semigroups

--- semigroups/tests/test_semigroup_loader.py
+++ semigroups/tests/test_semigroup_loader.py
@@ -33,7 +33,7 @@ class TestSemigroupLoader(unittest.TestCase):
             self.assertEqual(3, len(semigroup.table))
 
     def test_loads_order_four(self):
-        """Assert semigroups of order 5 are correctly loaded"""
+        """Assert semigroups of order 4 are correctly loaded"""
         order_4 = SemigroupLoader(Order.FOUR)
         self.assertEqual(order_4.order, Order.FOUR)
         self.assertIn(1, order_4.data.keys())
@@ -44,4 +44,44 @@ class TestSemigroupLoader(unittest.TestCase):
             self.assertEqual(key, semigroup.id)
             self.assertEqual(4, semigroup.order)
             self.assertEqual(4, len(semigroup.table))
+
+    def test_loads_order_five(self):
+        """Assert semigroups of order 5 are correctly loaded"""
+        order_5 = SemigroupLoader(Order.FIVE)
+        self.assertEqual(order_5.order, Order.FIVE)
+        self.assertIn(1, order_5.data.keys())
+        self.assertIn(1160, order_5.data.keys())
+        self.assertNotIn(1161, order_5.data.keys())
+        for key, semigroup in order_5.data.items():
+            self.assertIsInstance(semigroup, Semigroup)
+            self.assertEqual(key, semigroup.id)
+            self.assertEqual(5, semigroup.order)
+            self.assertEqual(5, len(semigroup.table))
+
+    def test_loads_order_six(self):
+        """Assert semigroups of order 6 are correctly loaded"""
+        order_6 = SemigroupLoader(Order.SIX)
+        self.assertEqual(order_6.order, Order.SIX)
+        self.assertIn(1, order_6.data.keys())
+        self.assertIn(15973, order_6.data.keys())
+        self.assertNotIn(15974, order_6.data.keys())
+        for key, semigroup in order_6.data.items():
+            self.assertIsInstance(semigroup, Semigroup)
+            self.assertEqual(key, semigroup.id)
+            self.assertEqual(6, semigroup.order)
+            self.assertEqual(6, len(semigroup.table))
+
+    @unittest.skip("Semigroups of order 7 not loaded yet.")
+    def test_loads_order_seven(self):
+        """Assert semigroups of order 7 are correctly loaded"""
+        order_7 = SemigroupLoader(Order.SEVEN)
+        self.assertEqual(order_7.order, Order.SEVEN)
+        self.assertIn(1, order_7.data.keys())
+        self.assertIn(15973, order_7.data.keys())
+        self.assertNotIn(15974, order_7.data.keys())
+        for key, semigroup in order_7.data.items():
+            self.assertIsInstance(semigroup, Semigroup)
+            self.assertEqual(key, semigroup.id)
+            self.assertEqual(6, semigroup.order)
+            self.assertEqual(6, len(semigroup.table))
     
